995fb9ded67fce18d8629c04549c6a81533ae785
Jānis Kosītis <janis.kositis@lnb.lv>
2023-04-26 19:23:45 +0300

    Chapter 4 practice done

---
+++ Chapter_4/printGrid.py
@@ -0,0 +1,19 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+def printGrid(gridToPrint) :
+    x = 0
+    y = 0
+    for y in range(len(gridToPrint[x])) :
+        for x in range(len(gridToPrint)) :
+            print(gridToPrint [x][y], end = '')
+        print()
+
+printGrid(grid)
